b303c268da884f571ea5e2c7004de1731237de3d
Eddie <edudek@gmail.com>
2021-05-21 18:21:38 -0500

    Tested version with the spreadsheet

--- python_service/ferm_track.py
+++ python_service/ferm_track.py
@@ -2,9 +2,7 @@
 
 # TODO UPDATE REFERENCES/SOURCES - ADD README
 # Ambient sensor: https://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-the-raspberry-pi/
-# send to queue/sb/whatever
 # https://blog.softhints.com/python-convert-object-to-json-3-examples/#built
-
 
 import sys
 import datetime
@@ -19,11 +17,6 @@ import adascreen
 import fermdatapoint
 import requests
 from copy import copy
-#from azure.servicebus import ServiceBusClient, ServiceBusMessage
-
-#lol hardcoded
-#CONNECTION_STR = 
-#QUEUE_NAME = "brewing-update"
 
 # DHT Sensor Type
 sensor = Adafruit_DHT.DHT22
@@ -33,7 +26,7 @@ gpio_pin = 26 # I think this is GPIO pin not literal pin #
 dpList = []
 
 CURRENT_BEER_NAME = "TestBeer1"
-SHEETS_SCRIPT = "<insert here>"
+SHEETS_SCRIPT = "<google sheet appender script>"
 
 def ScreenLoop(fermdp):
     while True:
@@ -65,9 +58,9 @@ def readAmbientSensor():
 def aggregateFermDPs(fermdp):
     dpList.append(copy(fermdp))
     time_diff = (fermdp.getTimestamp() - dpList[0].getTimestamp()).seconds
-    print("Time diff: " + str(time_diff))
+    #print("Time diff: " + str(time_diff))
     #Enough time has passed, aggregate all the datapoints and record it
-    if time_diff > 20: #Update this for real value aggregation
+    if time_diff >= 900: #Update this for real value aggregation
         gravity = 0
         fTemp = 0
         aTemp = 0
@@ -81,22 +74,23 @@ def aggregateFermDPs(fermdp):
         fTemp = fTemp / len(dpList)
         aTemp = aTemp / len(dpList)
         humidity = humidity / len(dpList)
-        print("Aggregated Data Point...")
-        print("T = " + str(fermdp.getTimestamp()))
-        print("G = " + "{:4.3f}".format(gravity))
-        print("F = " + "{:3.1f}".format(fTemp))
-        print("A = " + "{:3.1f}".format(aTemp))
+        #print("Aggregated Data Point...")
+        #print("T = " + str(fermdp.getTimestamp()))
+        #print("G = " + "{:4.3f}".format(gravity))
+        #print("F = " + "{:3.1f}".format(fTemp))
+        #print("A = " + "{:3.1f}".format(aTemp))
         senddata = {
-            'Time': fermdp.getTimestamp(),
+            'Timestamp': fermdp.getTimestamp(),
             'SG': "{:4.3f}".format(gravity),
-            'Temp': "{:3.1f}".format(fTemp),
+            'FermTemp': "{:3.1f}".format(fTemp),
+            'AmbTemp': "{:3.1f}".format(aTemp),
             'Color': "Purple",
-            'Beer': CURRENT_BEER_NAME,
-            'Comment': "{:3.1f}".format(aTemp)
+            'Humidity': humidity,
+            'Name': CURRENT_BEER_NAME,
+            'Comment': ""
             }
         r = requests.post(SHEETS_SCRIPT, senddata)
-        print(r) #???
-        dpList.clear()
+        print("Gravity: " + "{:4.3f}".format(gravity) + " Post Response: " + r)
         dpList.clear()
     
 def monitor_tilt():
@@ -115,11 +109,9 @@ def monitor_tilt():
             #get ambient
             humidity, ambtemp = readAmbientSensor()
             fermdp.updateFermPoint(beacon['color'], beacon['temp'], ambtemp, beacon['grav'], humidity, datetime.datetime.now())
-            #publish
             #print(fermdp.toJSON())
             aggregateFermDPs(fermdp)
-            #sender.send_messages(ServiceBusMessage(fermdp.toJSON()))
-        time.sleep(2) #for now just get an update every ~5s
+        time.sleep(10)
 
 if __name__ == '__main__':
     dev_id = 0
@@ -132,7 +124,5 @@ if __name__ == '__main__':
 
     blescan.hci_le_set_scan_parameters(sock)
     blescan.hci_enable_le_scan(sock)
-    #servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=True)
-    #with servicebus_client: #tab the monitor_tilt if using sb
     monitor_tilt()
     
